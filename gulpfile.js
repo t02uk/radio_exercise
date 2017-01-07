192f5485be327ec826983a9eb913291e2340469d
t02uk <tetsuya@mani.local>
2017-01-07 18:54:57 +0900

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var webserver = require('gulp-webserver');
+
+gulp.task('browserify', function() {
+  browserify('./src/index.js', { debug: true })
+    .transform(babelify)
+    .bundle()
+    .on("error", function (err) { console.log("Error : " + err.message); })
+    .pipe(source('index.js'))
+    .pipe(gulp.dest('./dest'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/*.js', ['browserify'])
+});
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      host: '127.0.0.1',
+      livereload: true
+    })
+  );
+});
+
+gulp.task('default', ['browserify', 'watch', 'webserver']);
